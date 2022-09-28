36e347a9cab915cb996bedc7b5e4400f9cb232dc
gabriele1245789 <riggi440@gmail.com>
2022-09-28 17:54:39 +0200

    definizione overloeading << classe punteggio

---
+++ punteggio.h
@@ -0,0 +1,48 @@
+#ifndef PUNTEGGIO_H
+#define PUNTEGGIO_H
+#include<iostream>
+#include<string>
+using namespace std;
+
+class punteggio{
+    public:
+        string cognome,nome,circolo;
+        double punti;
+        int anno;
+        punteggio(string cognome,string nome,string circolo,double punti,int anno){
+            this->cognome=cognome;
+            this->nome=nome;
+            this->circolo=circolo;
+            this->punti=punti;
+            this->anno=anno;
+        }
+        double getPunti(){
+            return this->punti;
+        }
+        string getNome(){
+            return this->nome;
+        }
+        string getCognome(){
+            return this->cognome;
+        }
+        string getCircolo(){
+            return this->circolo;
+        }
+        int getAnno(){
+            return this->anno;
+        }
+        friend ostream& operator <<(ostream& os,punteggio& obj){
+            os<<"nome:"<<obj.getNome()<<endl;
+            os<<"cognome:"<<obj.getCognome()<<endl;
+            os<<"circolo:"<<obj.getCircolo()<<endl;
+            os<<"punteggio:"<<obj.getPunti()<<endl;
+            os<<"anno di nascita:"<<obj.getPunti()<<endl;
+            return os;
+        }
+        
+
+
+
+
+};
+#endif
