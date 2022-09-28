972e1c0084dda34f6a4ae073e664a2c92d094834
gabriele1245789 <riggi440@gmail.com>
2022-09-28 17:29:05 +0200

    aggiunta classe nodo

---
+++ nodo.h
@@ -0,0 +1,33 @@
+#ifndef NODE_H
+#define NODE_H
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Node{
+   private:
+    T val;
+    Node<T>* next;
+    bool verbose;
+    
+    template<typename U> 
+    friend class List;   
+        public:
+            Node(T v, bool _verbose=true) : val(v), next(nullptr), verbose(_verbose) { }
+
+            Node(T v, Node<T>* n, bool _verbose=true) : val(v), next(n), verbose(_verbose) { }
+            
+            T& getVal(){ return this->val; }
+
+            Node<T>* getNext() const { return this->next; }
+            
+            friend ostream& operator<<(ostream& out,Node<T>& node){
+                if(node.verbose)
+                    out << "value = " << node.val;
+
+                return out;
+            }
+    
+};
+
+#endif
