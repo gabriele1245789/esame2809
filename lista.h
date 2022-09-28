779f4ea2d5290be932145aa608ce62d504295b6d
gabriele1245789 <riggi440@gmail.com>
2022-09-28 17:42:29 +0200

    aggiunta metodi inserimento lista

--- lista.h
+++ lista.h
@@ -20,6 +20,44 @@ class List{
             List(Node<T>* h) : head(h), tail(h) { }
 
             List(Node<T>* h, Node<T>* t) : head(h), tail(t) { }
+
+                Node<T>* getHead() const { return this->head; }
+
+    Node<T>* getTail() const { return this->tail; }
+    
+    bool isEmpty() const { return (head == nullptr) && (tail == nullptr); }
+    
+    void insert(T val){ //inserimento lista vuota
+        if(this->isEmpty()){
+          head = new Node<T>(val,verbose);
+          tail = head;
+        }
+
+        counter++;
+    }
+
+    void insertHead(T val){
+        if(this->isEmpty()){
+            this->insert(val);
+            return;
+        }  
+        Node<T>* temp = new Node<T>(val,verbose); //creo nodo
+        temp->next = head;  //imposto il successivo dell nodo alla vecchia testa
+        this->head = temp;  //aggiorno la testa della lista col nuovo nodo
+         
+        counter++;
+    }
+
+    void insertTail(T val){
+        if(this->isEmpty()){
+            this->insert(val);
+            return;
+        }
+        Node<T>* temp = new Node<T>(val,verbose); //creo nuovo nodo
+        tail->next = temp; //imposto il successivo dell'ultimo elemento al nuovo nodo
+        tail = temp; //aggiorno la coda
+        counter++;
+    }
     };
 
 #endif
